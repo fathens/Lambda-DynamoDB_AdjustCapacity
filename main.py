bee33d5ab82cdf008c8c4d37920f3f60e8fadb38
Kunio Sawatani <devel@fathens.org>
2016-01-26 10:54:02 +0900

    実際に実行してバグを修正

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import boto3
 import json
 import logging
+import math
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -12,39 +13,42 @@ def lambda_handler(event, context):
     message = json.loads(event['Records'][0]['Sns']['Message'])
     logger.info("Message: " + str(message))
 
-    if message['NewStateValue'] != 'ALARM':
-        context.succeed('NotAlarm')
-    else:
-        dynamodb = boto3.resource('dynamodb')
-
-        def metricKey():
-            metric = message['Trigger']['MetricName']
-            if metric == 'ConsumedReadCapacityUnits':
-                return 'ReadCapacityUnits'
-            elif metric == 'ConsumedWriteCapacityUnits':
-                return 'WriteCapacityUnits'
+    if message['NewStateValue'] == 'ALARM':
+        metricKeys = {
+            'ConsumedReadCapacityUnits': 'ReadCapacityUnits',
+            'ConsumedWriteCapacityUnits': 'WriteCapacityUnits'
+        }
+        metricKey = metricKeys[message['Trigger']['MetricName']]
 
         def dimension(name):
             list = message['Trigger']['Dimensions']
-            return next(map(lambda x: x['value'], filter(lambda x: x['name'] == name, list)), None)
+            return next(iter(map(lambda x: x['value'], filter(lambda x: x['name'] == name, list))), None)
 
-        def updateThroughput(throuput):
-            key = metricKey()
-            throuput[key] = throuput[key] * 1.2
-            return throuput
-
+        dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table(dimension('TableName'))
         indexName = dimension('GlobalSecondaryIndexName')
+
+        def enhance(src):
+            dst = int(math.ceil(src * 1.2))
+            logger.info("Enhance %s(%s) Throughput['%s']: %s => %s" % (table.name, indexName, metricKey, src, dst))
+            return dst
+
+        def updateThroughput(throughput):
+            map = {}
+            for name in metricKeys.values():
+                map[name] = throughput[name]
+
+            map[metricKey] = enhance(map[metricKey])
+            return map
+
         if indexName == None:
             table.update(ProvisionedThroughput=updateThroughput(table.provisioned_throughput))
         else:
-            index = next(filter(lambda x: x['IndexName'] == indexName, table.global_secondary_indexes), None)
+            index = next(iter(filter(lambda x: x['IndexName'] == indexName, table.global_secondary_indexes)), None)
             if index == None:
-                context.fail('No index: ' + indexName);
-            else:
-                table.update(GlobalSecondaryIndexUpdates=[{'Update': {
-                                                                      'IndexName': indexName,
-                                                                      'ProvisionedThroughput': updateThroughput(index['ProvisionedThroughput'])
-                                                                      }
-                                                           }])
-        context.succeed('OK')
+                raise Exception('No index: ' + indexName)
+            update = {
+                'IndexName': indexName,
+                'ProvisionedThroughput': updateThroughput(index['ProvisionedThroughput'])
+            }
+            table.update(GlobalSecondaryIndexUpdates=[{'Update': update}])
