3a420253ed6a3a4933fe979e63c10ba18ff51ff2
Kunio Sawatani <devel@fathens.org>
2016-01-28 11:48:52 +0900

    開発用バージョン（奇数）にしておく

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-logger.info("Version 2.0.0")
+logger.info("Version 2.0.1")
 
 cloudwatch = boto3.client('cloudwatch')
 
